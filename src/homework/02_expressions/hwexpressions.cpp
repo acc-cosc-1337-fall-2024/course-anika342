fdaabae8b5067fce688554830a51e8cafeb94a8a
anika342 <anikahuqsiraze.shailee@g.austincc.edu>
2024-09-15 04:24:23 +0000

    Hw - 2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -11,5 +11,5 @@ double get_sales_tax_amount(double meal_amount)
 
 double get_tip_amount(double meal_amount, double tip_rate)
 {
-    return meal_amount * tip_rate;
+    return meal_amount * (tip_rate/100.00);
 }
