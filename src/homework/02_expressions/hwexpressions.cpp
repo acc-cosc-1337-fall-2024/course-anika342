851a052bd742f79b580c2ef71f4c0d1a831d14eb
anika342 <anikahuqsiraze.shailee@g.austincc.edu>
2024-09-15 03:59:58 +0000

    Homework 2!

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -1,13 +1,15 @@
 #include "hwexpressions.h"
 
-//example
-int add_numbers(int num1, int num2)
-{
-	return num1 + num2;
-}
 
 //write function code here
 
+double get_sales_tax_amount(double meal_amount)
+{
+	const double tax_rate = 0.0675;
+    return meal_amount * tax_rate;
+}
 
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+    return meal_amount * tip_rate;
+}
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,31 @@
 ﻿//write include statements
+#include "hwexpressions.cpp"
+#include <iostream>
 
 
 //write namespace using statement for cout
+using namespace std;
 
 int main()
-{
+{ 
+	double meal_amount = 0.0; 
+	double tip_rate = 0.0;
+	double tip_amount = 0.0;
+	double tax_amount = 0.0;
+	double total = 0.0;
 	
+	cout << "Meal Amount:   " ;
+	cin >> meal_amount;
+	tax_amount = get_sales_tax_amount(meal_amount);
+	cout << "Sales Tax:     " << tax_amount << endl;
 
+	cout << "Enter tip % :  " ;
+	cin >> tip_rate;
+	tip_amount = get_tip_amount(meal_amount,tip_rate);
+	cout << "Tip Amount:    " << tip_amount << endl;;
+
+	total = meal_amount + tip_amount + tax_amount;
+
+	cout << "Total:         " << total << endl;
 	return 0;
 }
