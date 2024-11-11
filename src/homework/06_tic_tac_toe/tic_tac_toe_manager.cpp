ef4982b1b4c2fbee18c4493c4bf0d9680edb88ba
anika342 <anikahuqsiraze.shailee@g.austincc.edu>
2024-11-11 04:36:02 +0000

    Hw 8

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.cpp
@@ -1,1 +1,24 @@
 //cpp
+#include "tic_tac_toe_manager.h"
+
+using std::cout;
+
+void save_game(TicTacToe b)
+{
+
+}
+
+void get_winner_total(int& o, int& w, int& t)
+{
+    o = o_win;
+    w = x_win;
+    t = ties;
+
+}
+
+//private functions
+void update_winner_count(string winner)
+{
+
+}
+
--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,23 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+class TicTacToeManager
+{
+public: 
+    void save_game(TicTacToe b);
+    void get_winner_total(int& o, int& w, int& t);
+
+private:
+    std::vector<std::string> games{TicTacToe};
+    int x_win = 0;
+    int o_win = 0;
+    int ties = 0;
+
+    void update_winner_count(string winner);
+};
+
+#endif
